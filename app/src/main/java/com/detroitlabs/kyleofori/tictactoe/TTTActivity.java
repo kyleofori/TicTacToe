7fd2462310db0300feaf38688eb0fdc1343e90be
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-10-05 21:59:27 -0400

    Adding PlayOnClick class

--- app/src/main/java/com/detroitlabs/kyleofori/tictactoe/TTTActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/tictactoe/TTTActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TableRow;
 
@@ -13,13 +14,14 @@ public class TTTActivity extends Activity {
     private boolean xTurn = false;
     private char[][] gameBoard = new char[3][3];
 
+
     private void setUpOnClickListeners() {
         LinearLayout L = (LinearLayout) findViewById(R.id.linearLayout);
         for(int y = 0; y < L.getChildCount(); y++) {
-            if(L.getChildAt(y) instanceof TableRow) {
-                TableRow R = (TableRow) L.getChildAt(y);
-                for(int x = 0; x < R.getChildCount(); x ++) {
-                    View V = R.getChildAt(x); // In our case this will be each button on the grid
+            if(L.getChildAt(y) instanceof LinearLayout) {
+                LinearLayout innerL = (LinearLayout) L.getChildAt(y);
+                for(int x = 0; x < innerL.getChildCount(); x+=2) {
+                    View V = L.getChildAt(x); // In our case this will be each button on the grid
                     V.setOnClickListener(new PlayOnClick(x, y));
                 }
             }
@@ -27,6 +29,27 @@ public class TTTActivity extends Activity {
     }
 
 
+    private class PlayOnClick implements View.OnClickListener {
+
+        private int x = 0;
+        private int y = 0;
+
+        public PlayOnClick(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public void onClick(View view) {
+            if(view instanceof Button) {
+                Button B = (Button) view;
+                gameBoard[x][y] =  xTurn ? 'O' : 'X';
+                B.setText(xTurn ? "O" : "X");
+                B.setEnabled(false);
+                xTurn = !xTurn;
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
